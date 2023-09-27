2928e24e176eced2aa5c9a53c545592fd67de3ed
Tim Zwiebel <timzwiebel@gmail.com>
2023-09-27 15:30:37 -0700

    Add `.svelte-kit` to excluded_search_dirs.sh

--- lib/shell/excluded_search_dirs.sh
+++ lib/shell/excluded_search_dirs.sh
@@ -5,5 +5,6 @@
 
 _EXCLUDED_SEARCH_DIRS=('.git')
 _EXCLUDED_SEARCH_DIRS+=('.history')
+_EXCLUDED_SEARCH_DIRS+=('.svelte-kit')
 _EXCLUDED_SEARCH_DIRS+=('node_modules')
 readonly _EXCLUDED_SEARCH_DIRS
